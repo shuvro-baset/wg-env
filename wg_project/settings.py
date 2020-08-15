be30309b6fdd9e98b8dd11ae593adc16fce738db
forhadisrafil <forhadtuhin5088@gmail.com>
2020-08-16 02:28:15 +0600

    template issue wip

--- wg_project/settings.py
+++ wg_project/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'wg_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'templates/')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
